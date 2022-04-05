243a1dae3f000e9a69d6c58b97a029a2a8826f29
lixianyang <lixianyang@meituan.com>
2022-04-05 22:45:03 +0800

    update

--- ros/src/learning_topic/scripts/pose_subscriber.py
+++ ros/src/learning_topic/scripts/pose_subscriber.py
@@ -1,15 +1,17 @@
-#! /home/lixianyang/miniconda3/envs/a100/bin/python
+#!/home/lixianyang/miniconda3/bin/python
 import rospy
 from turtlesim.msg import Pose
 
+
 def poseCallback(msg):
-    rospy.loginfo('pose x: %.2f %.2f',msg.x,msg.y)
+    rospy.loginfo('pose x: %.2f %.2f', msg.x, msg.y)
+
 
 def main():
     rospy.init_node('pose_subscriber')
-    rospy.Subscriber('/turtle1/pose',Pose,poseCallback)
+    rospy.Subscriber('/turtle1/pose', Pose, poseCallback)
     rospy.spin()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
